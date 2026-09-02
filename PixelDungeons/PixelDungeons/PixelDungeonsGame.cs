beeae589a9f5f9e8b329871a474a41eaeb1c90e1
Johanny Velandia <4nuquet@gmail.com>
2026-09-01 20:05:38 -0500

    Add PixelDungeons project

---
+++ PixelDungeons/PixelDungeons/PixelDungeonsGame.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using PixelEngine;
+
+namespace PixelDungeons;
+
+public class PixelDungeonsGame : EngineGame
+{
+    public PixelDungeonsGame(): base("Pixel Dungeons", 1920, 1080, false)
+    {
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        // TODO: use this.Content to load your game content here
+        base.LoadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Magenta);
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
---
+++ PixelDungeons/PixelEngine/PixelEngine.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PixelEngine;
+
+public abstract class EngineGame : Game
+{
+    internal static EngineGame s_instance;
+
+    /// <summary>
+    /// Gets a reference to the EngineGame instance.
+    /// </summary>
+    public static EngineGame Instance => s_instance;
+
+    /// <summary>
+    /// Gets the graphics device manager to control the presentation of graphics.
+    /// </summary>
+    public static GraphicsDeviceManager Graphics { get; private set; }
+
+    /// <summary>
+    /// Gets the graphics device used to create graphical resources and perform primitive rendering.
+    /// </summary>
+    public static new GraphicsDevice GraphicsDevice { get; private set; }
+
+    /// <summary>
+    /// Gets the sprite batch used for all 2D rendering.
+    /// </summary>
+    public static SpriteBatch SpriteBatch { get; private set; }
+
+    /// <summary>
+    /// Gets the content manager used to load global assets.
+    /// </summary>
+    public static new ContentManager Content { get; private set; }
+
+    /// <summary>
+    /// Creates a new EngineGame instance.
+    /// </summary>
+    public EngineGame(string title, int width, int height, bool fullScreen)
+    {
+        if (s_instance != null)
+        {
+            throw new InvalidOperationException("Only a single EngineGame instance can be created.");
+        }
+
+        s_instance = this;
+
+        // Configuración de gráficos
+        Graphics = new GraphicsDeviceManager(this)
+        {
+            PreferredBackBufferWidth = width,
+            PreferredBackBufferHeight = height,
+            IsFullScreen = fullScreen
+        };
+        Graphics.ApplyChanges();
+
+        Window.Title = title;
+
+        // Content
+        Content = base.Content;
+        Content.RootDirectory = "Content";
+
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        // En MonoGame, GraphicsDevice y SpriteBatch deben inicializarse tras base.Initialize()
+        GraphicsDevice = base.GraphicsDevice;
+        SpriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+}
